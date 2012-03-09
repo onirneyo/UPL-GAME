b1f90adca4e8c0882322815e94eaf53fffc5933e
Alex Sohail <asohail@wisc.edu>
2012-03-09 17:45:17 -0600

    Displays UPL Logo

--- MazeGame/src/com/upl/mazegame/Game.java
+++ MazeGame/src/com/upl/mazegame/Game.java
@@ -1,13 +1,19 @@
 package com.upl.mazegame;
 
+import com.badlogic.*;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game implements ApplicationListener {
-
+	private SpriteBatch batch;
+	private Texture texture;
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
-
+		batch = new SpriteBatch();
 	}
 
 	@Override
@@ -18,8 +24,13 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
 
+		texture = new Texture(Gdx.files.internal("uplogo.jpg"));
+		batch.begin();
+		batch.draw(texture, 10, 10);
+		batch.end();
+		
+	  
 	}
 
 	@Override
