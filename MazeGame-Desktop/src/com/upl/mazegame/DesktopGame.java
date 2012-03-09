87445dbaeb0fea5de06cf096183228194df47e21
Alex Sohail <asohail@wisc.edu>
2012-03-09 15:32:30 -0600

    Initial Commit

---
+++ MazeGame-Desktop/src/com/upl/mazegame/DesktopGame.java
@@ -0,0 +1,8 @@
+package com.upl.mazegame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class DesktopGame {
+    public static void main (String[] args) {
+            new LwjglApplication(new Game(), "Game", 480, 320, false);
+    }
+}
